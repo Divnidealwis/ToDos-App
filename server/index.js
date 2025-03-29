494e66ed8da25711bcffd9ae4b12be69d97a5a27
Divni de Alwis <divnidealwis@gmail.com>
2025-03-30 01:48:44 +0530

    feat: Connect backend to database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const { connectToMongoDB } = require("./database")
+
 const app = express();
 
 const router = require("./routes");
-
 app.use("/api", router);
 
+const port = process.env.PORT || 5001;
 
-const port = 5001;
-app.listen(port, () => {
-    console.log(`Server is listening on http://localhost:${port}`);
-})
+const startServer = async () => {
+    await connectToMongoDB();
+    app.listen(port, () => {
+        console.log(`Server is listening on http://localhost:${port}`);
+    });
+};
+
+startServer();
