fb63a88e704b4d0b0e511aa680b41a0cf084a361
Divni de Alwis <divnidealwis@gmail.com>
2025-03-30 03:33:31 +0530

    feat(validation): Add stricter validation for todos API

--- server/routes.js
+++ server/routes.js
@@ -15,6 +15,8 @@ router.get("/todos", async (req, res) => {
 router.post("/todos", async (req, res) => {
     const collection = getCollection();
     const { todo } = req.body;
+
+    todo = JSON.stringify(todo);
   
     const newTodo = await collection.insertOne({ todo, status: false });
   
@@ -35,6 +37,10 @@ router.put("/todos/:id", async (req, res) => {
     const collection = getCollection();
     const _id = new ObjectId(req.params.id);
     const { status } = req.body;
+
+    if (typeof status !== "boolean") {
+        return res.status(400).json({ mssg: "invalid status" });
+    }
   
     const updatedTodo = await collection.updateOne({ _id }, { $set: { status: !status } });
     res.status(200).json(updatedTodo);
